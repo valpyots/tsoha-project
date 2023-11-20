0b91f41b353bf5c12dcee34602b7aeaf21c4f49f
Pyötsiä Valtteri O J <valpyots@cubbli22-gold.local>
2023-11-20 16:30:03 +0200

    Updated SQL schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE categories (ID SERIAL PRIMARY KEY, name TEXT);
-CREATE TABLE messages (ID SERIAL PRIMARY KEY, content TEXT, user_id INTEGER REFERENCES users, sent_at TIMESTAMP, topic_id REFERENCES topics);
+CREATE TABLE messages (ID SERIAL PRIMARY KEY, content TEXT, user_id INTEGER REFERENCES users, sent_at TIMESTAMP, topic_id REFERENCES topics visible BOOLEAN);
 CREATE TABLE users (ID SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, profilevisible BOOLEAN, canpost BOOLEAN, isAdmin BOOLEAN);
 CREATE TABLE topics (ID SERIAL PRIMARY KEY message TEXT, title TEXT, user_id INTEGER REFERENCES users, sent_at TIMESTAMP, visible BOOLEAN, categoryid INTEGER  REFERENCES categories);
 
